faa8d1b152e50936003716b2e1a9216725163d43
Leonel Herrera <48606307+Leonel-H29@users.noreply.github.com>
2023-10-28 12:15:59 -0300

    Implemento los endpoints para obtener, descargar y remover un archivo

--- server/app/main/routes.py
+++ server/app/main/routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, UploadFile, File
-from os import getcwd
+from fastapi.responses import FileResponse, JSONResponse
+from os import getcwd, remove
 
 router = APIRouter()
 
@@ -21,3 +22,28 @@ async def upload_file(file: UploadFile= File(...)):
         Myfile.write(content)
         Myfile.close()
     return "Success"
+
+
+@router.get("/file/{name_file}")
+def get_file(name_file: str):
+    return FileResponse(getcwd() + "/" + name_file)
+
+@router.get("/download/{name_file}")
+def download_file(name_file: str):
+    mediaType ="application/octet-stream"
+    return FileResponse(getcwd() + "/" + name_file, media_type=mediaType, filename=name_file)
+
+@router.delete("/delete/{name_file}")
+def delete_file(name_file: str):
+    try:
+        remove(getcwd() + "/" + name_file)
+    except FileNotFoundError:
+        return JSONResponse(content={
+            "removed": False,
+            "messagge": "File Not Found"
+        }, status_code=404)
+
+    return JSONResponse(content={
+            "removed": True,
+            "messagge": "Ok"
+        }, status_code=200)
