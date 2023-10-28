673c1d8b847fd48c5ad3deb9822e3adf7d718f64
Leonel-H29 <48606307+Leonel-H29@users.noreply.github.com>
2023-10-28 20:18:56 -0300

    Creo el endpoint para obtener el listado de los directorios y archivos dentro de una ubicacion

--- server/app/main/routes.py
+++ server/app/main/routes.py
@@ -1,45 +1,29 @@
-from fastapi import APIRouter, UploadFile, File
-from fastapi.responses import FileResponse, JSONResponse
-from os import getcwd, remove
+from fastapi import APIRouter
+from os import listdir, path
+from fastapi import HTTPException
 
+URL = '/api/'
 router = APIRouter()
+
 
 @router.get("/")
 def main():
     return "Hello World!"
 
 
-# @router.post("/upload")
-# async def upload_file(file: UploadFile= File(...)):
+@router.get(URL+"list")
+def list_files_and_directories(location: str = "."):
+    try:
+        # Obtener la lista de archivos y directorios
+        contents = listdir(location)
 
-#     file_dir = getcwd() + "/" + file.filename
-#     with open(file_dir, "wb") as Myfile:
-#         content = await file.read()
-#         Myfile.write(content)
-#         Myfile.close()
-#     return "Success"
+        # Incluir archivos y directorios ocultos
+        all_contents = [path.join(location, item) for item in contents]
 
+        # Filtrar archivos y directorios ocultos
+        visible_contents = [
+            item for item in all_contents if not item.split('/')[-1].startswith('.')]
 
-# @router.get("/file/{name_file}")
-# def get_file(name_file: str):
-#     return FileResponse(getcwd() + "/" + name_file)
-
-# @router.get("/download/{name_file}")
-# def download_file(name_file: str):
-#     mediaType ="application/octet-stream"
-#     return FileResponse(getcwd() + "/" + name_file, media_type=mediaType, filename=name_file)
-
-# @router.delete("/delete/{name_file}")
-# def delete_file(name_file: str):
-#     try:
-#         remove(getcwd() + "/" + name_file)
-#     except FileNotFoundError:
-#         return JSONResponse(content={
-#             "removed": False,
-#             "messagge": "File Not Found"
-#         }, status_code=404)
-
-#     return JSONResponse(content={
-#             "removed": True,
-#             "messagge": "Ok"
-#         }, status_code=200)
+        return {"contents": visible_contents}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
