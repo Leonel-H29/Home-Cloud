286d1360a9e694c69aff804a3225e0787d433143
Leonel Herrera <leonelherreradkno@gmail.com>
2023-12-31 19:05:48 -0300

    Mejoro los estilos de la terminal

--- client/src/components/Terminal/Terminal.tsx
+++ client/src/components/Terminal/Terminal.tsx
@@ -1,22 +1,30 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import './Terminal.css';
 import { TerminalClass } from '../Class/TerminalClass';
+import React from 'react';
 
 const TerminalComponent = () => {
-  const [command, setCommand] = useState('');
-  const [output, setOutput] = useState('');
+  const [commands, setCommands] = useState<string[]>([]); // Almacena todos los comandos
+  const [output, setOutput] = useState<string[]>([]); // Almacena todas las salidas
+  const inputRef = useRef<HTMLInputElement>(null); // Referencia al input de comando
 
   const IShell = new TerminalClass();
 
-  const handleCommand = async () => {
+  useEffect(() => {
+    inputRef.current?.focus(); // Enfocar automáticamente el input al iniciar
+  }, []);
+
+  const handleCommand = async (command: string) => {
     try {
-      const data = IShell.Command(command);
-      //console.log('Response: ', response);
-      console.log('Data: ', await data);
-      setOutput(await data);
-      if (command == 'cd') {
-        IShell.setLocation(await data);
+      if (command == 'clear') {
+        setOutput([]);
+        setCommands([]);
+        return;
       }
+      const data = await IShell.Command(command);
+      setOutput((prevOutput) => [...prevOutput, data]); // Agregar la nueva salida debajo de la existente
+      setCommands((prevCommands) => [...prevCommands, command]); // Agregar el nuevo comando
+      inputRef.current?.focus(); // Enfocar automáticamente el input después de ejecutar el comando
     } catch (error) {
       console.error('Error executing command:', error);
     }
@@ -27,22 +35,52 @@ const TerminalComponent = () => {
       <div className="terminal">
         <div className="terminal-header">Terminal</div>
         <div className="terminal-body">
+          <div className="output-container">
+            {' '}
+            {output.map((out, index) => (
+              <React.Fragment key={index}>
+                <div className="input-line">
+                  <span className="green-text">:{IShell.GetLocation()}$</span>
+                  <input
+                    className="command-input"
+                    type="text"
+                    value={commands[index]}
+                    onChange={(e) => {
+                      const newCommands = [...commands];
+                      newCommands[index] = e.target.value;
+                      setCommands(newCommands);
+                    }}
+                    onKeyDown={(e) => {
+                      if (e.key === 'Enter') {
+                        handleCommand(commands[index]);
+                      }
+                    }}
+                    readOnly
+                  />
+                </div>
+                <pre className="output">{out}</pre>
+              </React.Fragment>
+            ))}
+          </div>
           <div className="input-line">
-            {IShell.GetLocation()}
-            <span className="green-text">$</span>
+            <span className="green-text">:{IShell.GetLocation()}$</span>
             <input
+              ref={inputRef}
               className="command-input"
               type="text"
-              value={command}
-              onChange={(e) => setCommand(e.target.value)}
+              value={commands[output.length] || ''}
+              onChange={(e) => {
+                const newCommands = [...commands];
+                newCommands[output.length] = e.target.value;
+                setCommands(newCommands);
+              }}
               onKeyDown={(e) => {
                 if (e.key === 'Enter') {
-                  handleCommand();
+                  handleCommand(commands[output.length] || '');
                 }
               }}
             />
           </div>
-          <pre className="output">{output}</pre>
         </div>
       </div>
     </div>
