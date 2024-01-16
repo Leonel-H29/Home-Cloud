36b29b5186f84a23b365df4f6676f76ff2692995
Leonel Herrera <leonelherreradkno@gmail.com>
2024-01-16 15:51:47 -0300

    Resuelvo problemas de entornos y de CORS

--- server/app/main/main_dev.py
+++ server/app/main/main_dev.py
@@ -1,21 +1,10 @@
-from pathlib import Path
-from app.main.main import *
-import os
-from dotenv import load_dotenv
-
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-print(BASE_DIR)
-
-
-# Load environment variables from .env file
-FILE_ENV = 'dev.env'
-load_dotenv(Path.joinpath(BASE_DIR, FILE_ENV))
-# ALLOWED_COMMANDS = os.getenv("ALLOWED_COMMANDS").split(",")
+from app.main.main import app
+from fastapi.middleware.cors import CORSMiddleware
 
 app.debug = True
 print(f"DEBUG: {app.debug}")
 origins = ['*']
-# DEBUG = app.debug
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
--- server/manage.py
+++ server/manage.py
@@ -3,25 +3,28 @@ import uvicorn
 
 
 def main():
-    # Define el módulo y el nombre de la aplicación de FastAPI que deseas ejecutar
-    # Reemplaza 'app.main.main:app' con el nombre de tu módulo y aplicación de FastAPI
-    module_name = 'app.main.main:app'
-    # Por defecto, usa el entorno de desarrollo
+        
+    # By default, use the development environment
     app_env = os.getenv("APP_ENV", "dev")
 
-    # Importa configuraciones específicas de entorno
-    if app_env == "dev":
-        from app.main.main_dev import app
-    elif app_env == "prod":
-        from app.main.main_prod import app
-    else:
-        raise ValueError("APP_ENV debe ser 'dev' o 'prod'")
-
-    # Define el puerto en el que deseas ejecutar el servidor
+    # Define the port on which you want to run the server
     port = 8082
-
+    # Enabled auto reload
     reload = True
-    uvicorn.run(module_name, host='0.0.0.0', port=port,
+    
+    
+    """
+    If the user passes the input 'ENV=prod' as a parameter, it will use the production environment. Otherwise it will use the development environment.
+    """
+    if app_env == "prod":
+        # Define the module and name of the FastAPI application you want to run
+        module_name = 'app.main.main_prod:app'
+        uvicorn.run(app=module_name, host='0.0.0.0', port=port,
+                reload=reload, log_level="info")
+    else:
+    
+        module_name = 'app.main.main_dev:app'
+        uvicorn.run(app=module_name, host='0.0.0.0', port=port,
                 reload=reload, log_level="info")
 
 
