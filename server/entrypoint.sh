5d8a024e9a0a8eccd068be96376dab89e8afdf38
Leonel-H29 <48606307+Leonel-H29@users.noreply.github.com>
2024-01-18 15:42:29 -0300

    Implemento volumen de archivos estaticos en Docker

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+##I look for directories with the name '__pycache__'
+directories=$(find $PWD -type d -name "__pycache__")
+
+#I go through the list and delete each directory and the contents of each directory
+for x in $directories; do
+	rm -r $x
+done
+
+if [ -d "static" ]; then
+
+    echo "Alredy exist directory to static files"
+
+else
+    echo "Building static files directory ..."
+    sleep 2
+    mkdir static
+fi
 
 echo "Uploading server ..."
 sleep 2
